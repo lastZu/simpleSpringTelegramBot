e886ecc2bc14413a4952e29883788f1b9b1a31bb
lastZu <cdrom2015@ya.ru>
2023-10-03 16:33:55 +0300

    add Logging

--- bot-maker/src/main/java/github/lastzu/answer/AnswerFactory.java
+++ bot-maker/src/main/java/github/lastzu/answer/AnswerFactory.java
@@ -1,7 +1,5 @@
 package github.lastzu.answer;
 
-import org.telegram.telegrambots.meta.api.objects.Update;
-
 public interface AnswerFactory {
     void setRequest(Request request);
 
--- dispatcher/src/main/java/github/lastzu/HandlingBot.java
+++ dispatcher/src/main/java/github/lastzu/HandlingBot.java
@@ -1,7 +1,9 @@
 package github.lastzu;
 
-import github.lastzu.answer.Answer;
 import github.lastzu.answer.AnswerFactory;
+
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
@@ -11,6 +13,7 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 public class HandlingBot extends TelegramLongPollingBot {
     private AnswerFactory answerFactory;
     private final String name;
+    Logger log = LoggerFactory.getLogger(HandlingBot.class);
 
     public HandlingBot(String name, String token) {
         this(name, token, new StartAnswerFactory());
@@ -24,10 +27,14 @@ public class HandlingBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
+        log.info("Bot received updates");
+
         answerFactory.setRequest(
                 getRequest(update)
         );
         sendAnswer();
+
+        log.info("Bot finished send response");
     }
 
     @Override
@@ -36,13 +43,15 @@ public class HandlingBot extends TelegramLongPollingBot {
     }
 
     private void sendAnswer() {
+        log.debug("Getting message");
         Response answer = answerFactory.getResponse();
         SendMessage message = getMessage(answer);
+
         try {
             execute(message);
+            log.debug("Message send successfully");
         } catch (TelegramApiException e) {
-            // TODO Logging
-            e.printStackTrace();
+            log.error("Can not send message", e);
         }
     }
 
